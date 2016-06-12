1bb52543a42b853a1056322861b868eaa2dfc8ae
lewjun <lewjun072@sina.com>
2016-06-12 23:47:48 +0800

    add slf4j

--- src/test/java/com/lewjun/AppTest.java
+++ src/test/java/com/lewjun/AppTest.java
@@ -1,16 +1,20 @@
 package com.lewjun;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+	private static final Logger LOGGER = LoggerFactory.getLogger(AppTest.class);
 	/**
 	 * 打个招呼 :-)
 	 */
 	@Test
 	public void testGreeting() {
-		System.out.println("Hello GitHub");
+//		System.out.println("Hello GitHub");
+		LOGGER.debug("Hello GitHub");
 	}
 }
