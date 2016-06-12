18b8c9364e95c2f08e9f49e685828283e36e5e0c
lewjun <lewjun072@sina.com>
2016-06-12 23:28:25 +0800

    first commit

---
+++ src/test/java/com/lewjun/AppTest.java
@@ -0,0 +1,16 @@
+package com.lewjun;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	/**
+	 * 打个招呼 :-)
+	 */
+	@Test
+	public void testGreeting() {
+		System.out.println("Hello GitHub");
+	}
+}
