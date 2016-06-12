7d04357e590f4f83eea62f98bb1c70a3e224953c
lewjun <lewjun072@sina.com>
2016-06-13 00:46:30 +0800

    add Logger to App

--- src/main/java/com/lewjun/App.java
+++ src/main/java/com/lewjun/App.java
@@ -1,11 +1,17 @@
 package com.lewjun;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App {
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
 	public static void main(String[] args) {
-		System.out.println("Hello GitHub!");
+		// System.out.println("Hello GitHub!");
+		LOGGER.debug("Hello {}!", "GitHub");
 	}
 }
